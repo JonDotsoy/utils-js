c5e5b3d9f97bfdd3fbc96b6126a4758fe40db9da
Jonathan Delgado <hi@jon.soy>
2024-09-12 16:42:09 -0300

    feat: add functionality to set.ts for setting values at specified path within a nested object structure and providing example usage in JSDoc comments
    ```javascript
    /**
    * Sets a value at a specified path within a nested object structure.
    *
    * @param {unknown} obj - The object to modify.
    * @param {PropertyKey[]} paths - An array of property keys representing the path to the target property.
    * @param {unknown} value - The value to set at the specified path.
    * @returns {unknown} The modified object.
    */
    export const set = (obj: unknown, paths: PropertyKey[], value: unknown) => {
        // implementation...
    };
    ```

--- src/set/set.ts
+++ src/set/set.ts
@@ -1,5 +1,3 @@
-import { get } from "../get/get.js";
-
 const pick = (
   obj: unknown,
   paths: PropertyKey[],
@@ -18,6 +16,24 @@ const pick = (
   });
 };
 
+/**
+ * Sets a value at a specified path within a nested object structure.
+ *
+ * @param {unknown} obj - The object to modify.
+ * @param {PropertyKey[]} paths - An array of property keys representing the path to the target property.
+ * @param {unknown} value - The value to set at the specified path.
+ * @returns {unknown} The modified object.
+ *
+ * @example
+ * const data = { a: { b: 1 } };
+ * // Sets the value at 'b' within the nested object 'a' to 2
+ * set(data, ['a', 'b'], 2);
+ * console.log(data); // { a: { b: 2 } }
+ *
+ * // Creates an object at 'c' if it doesn't exist, and sets its 'd' property to 3
+ * set(data, ['a', 'c', 'd'], 3);
+ * console.log(data); // { a: { b: 2, c: { d: 3 } } }
+ */
 export const set = (obj: unknown, paths: PropertyKey[], value: unknown) => {
   const o = { obj };
   const lastChild = pick(o.obj, paths, o, (val) => {
