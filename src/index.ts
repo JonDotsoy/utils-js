ef9c76c8d38ff017b82ade7acfbedcc9892e5f31
Jonathan Delgado <hi@jon.soy>
2024-09-23 15:55:41 -0300

    fix: export `result` and `SymbolResult` on main module

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { get } from "./get/get.js";
 export { set } from "./set/set.js";
 export { visit } from "./visit/visit.js";
 export { pipe } from "./pipe/pipe.js";
+export { result } from "./result/result.js";
+export { SymbolResult } from "./symbol-result/symbol-result.js";
