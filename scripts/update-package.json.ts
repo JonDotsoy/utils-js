853da5bbd0b561b5a287e10a7ac6158b7e5670ce
Jonathan Delgado <hi@jon.soy>
2024-09-24 11:39:40 -0300

    perf: upgrade strategy to build export files

--- scripts/update-package.json.ts
+++ scripts/update-package.json.ts
@@ -1,3 +1,4 @@
+import { pipe } from "../src/pipe/pipe.js";
 import { Glob } from "bun";
 import * as path from "path";
 // import pkg from "../package.json";
@@ -8,30 +9,80 @@ const pkg = await fs.readFile(pkgLocation, "utf-8").then(JSON.parse);
 pkg.exports ??= {};
 const importAliases: Record<string, string> = { index: "." };
 
-async function ds(pattern: string, cwd: string, ext: string, t: string) {
+const globModules = (cwd: string, pattern: string) => {
   const glob = new Glob(pattern);
 
-  const esm_libs_dir = new URL(cwd, import.meta.url);
+  const pathCwd = new URL(cwd, import.meta.url);
 
-  for await (const elm of glob.scan({
-    cwd: esm_libs_dir.pathname,
-    absolute: true,
-  })) {
-    const relative = path.relative(new URL("./", pkgLocation).pathname, elm);
-    const basename = path.basename(relative, ext);
-    const exportKey = importAliases[basename] ?? `./${basename}`;
-    Reflect.set(
-      pkg.exports,
-      exportKey,
-      Reflect.get(pkg.exports, exportKey) ?? {},
-    );
-    Reflect.set(Reflect.get(pkg.exports, exportKey), t, `./${relative}`);
-  }
+  return Array.fromAsync(
+    glob.scan({
+      cwd: pathCwd.pathname,
+      absolute: true,
+      onlyFiles: false,
+      dot: false,
+      followSymlinks: false,
+    }),
+    async elementPath => {
+      const stat = await fs.stat(elementPath);
+      const isDirectory = stat.isDirectory();
+      const extname = path.extname(elementPath);
+      const name = path.basename(elementPath, extname);
+
+      return {
+        name,
+        isDirectory,
+        elementPath,
+      }
+    }
+  )
 }
 
-await ds("**/*.js", "../libs/esm/", ".js", "import");
-await ds("**/*.d.ts", "../libs/types/", ".d.ts", "types");
-//
+const pkgExports =
+  await pipe(globModules("../src/", "*"))
+    .pipe(list => Array.fromAsync(list, async ({ name }) => {
+      const findLibs = async (name: string) => {
+        const esmLib = await Array.fromAsync([
+          new URL(`../libs/esm/${name}/${name}.js`, import.meta.url),
+          new URL(`../libs/esm/${name}.js`, import.meta.url),
+        ], async (location) => {
+          const exists = await fs.exists(location);
+          return exists ? location.toString() : null;
+        });
+
+        const typeLib = await Array.fromAsync([
+          new URL(`../libs/types/${name}/${name}.d.ts`, import.meta.url),
+          new URL(`../libs/types/${name}.d.ts`, import.meta.url),
+        ], async (location) => {
+          const exists = await fs.exists(location);
+          return exists ? location.toString() : null;
+        });
+
+        return {
+          esm: esmLib.find(Boolean),
+          type: typeLib.find(Boolean),
+        }
+      }
+
+      return ({
+        name,
+        libs: await findLibs(name),
+      });
+    }))
+    .pipe(list => Array.fromAsync(list, async (info) => {
+      const relative = (location?: string | null) => {
+        return location ? `./${path.relative(new URL("./", pkgLocation).pathname, new URL(location).pathname)}` : undefined
+      }
+      return [importAliases[info.name] ?? `./${info.name}`, {
+        import: relative(info.libs.esm),
+        types: relative(info.libs.type),
+      }]
+    }))
+    .pipe(list => list.sort(([a], [b]) => a > b ? 1 : -1))
+    .pipe(list => Object.fromEntries(list))
+    .value()
+
+Reflect.set(pkg, "exports", pkgExports);
+
 // console.log("🚀 ~ pkg:", pkg)
 const writeMode = process.argv.indexOf("--write") !== -1;
 if (writeMode) {
