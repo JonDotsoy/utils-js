d8895cf56d92e4b95099650587f010ce3f7cc05b
Jonathan Delgado <hi@jon.soy>
2025-05-21 21:37:56 -0400

    feat(query): enhance Query class with improved filtering methods and utility functions

--- src/query/query.ts
+++ src/query/query.ts
@@ -1,38 +1,69 @@
-// Utilidad query para simplificar búsquedas recursivas sobre estructuras de datos
+import get from "../get/get.js";
 
+/**
+ * Represents a function that evaluates a condition on a given value of type `T`.
+ * Returns `true` if the value satisfies the condition, otherwise `false`.
+ *
+ * @typeParam T - The type of the value to be tested by the predicate.
+ * @param node - The value to test.
+ * @returns `true` if the value satisfies the condition, otherwise `false`.
+ */
 export type Predicate<T> = (node: T) => boolean;
 
 export default class Query<T> {
   #predicates = new Set<Predicate<T>>();
+  #selectProps: string[] = [];
 
   constructor() {}
 
   /**
    * Filtra los nodos por instancia de clase
    */
-  instanceOf<T>(cls: new (...args: any[]) => T): this {
-    this.where((node) => {
+  instanceOf<T>(cls: new (...args: any[]) => T) {
+    return this.where((node) => {
       return node instanceof cls;
     });
-    return this;
   }
 
   /**
    * Filtra los nodos por predicado
    */
-  where(predicate: Predicate<T>): this {
-    this.#predicates.add(predicate);
-    return this;
+  where<A = T>(predicate: Predicate<A>): Query<A> {
+    const query = new Query();
+    this.#predicates.forEach((p) => query.#predicates.add(p as any));
+    query.#predicates.add((node: any) => {
+      const selectedNode = get(node, ...this.#selectProps) as any;
+      const predicateResult = predicate(selectedNode);
+
+      return predicateResult;
+    });
+    query.#selectProps = [...query.#selectProps];
+    return query;
   }
 
   /**
    * Filtra los nodos que tienen una propiedad específica
    */
-  hasProperty(prop: string): this {
-    return this.where(
-      (node: any) =>
-        node != null && Object.prototype.hasOwnProperty.call(node, prop),
-    );
+  hasProperty(prop: string) {
+    const query = this.where((node: any) => {
+      // console.log("node~",node)
+      return node != null && Object.prototype.hasOwnProperty.call(node, prop);
+    });
+    query.#selectProps = [...this.#selectProps, prop];
+    return query;
+  }
+
+  equal(value: any) {
+    return this.where((node) => {
+      return node === value;
+    });
+  }
+
+  match(matcher: string | RegExp) {
+    return this.where((node: any) => {
+      if (typeof node === "string") return node.match(matcher) !== null;
+      return false;
+    });
   }
 
   static size(query: Query<any>) {
