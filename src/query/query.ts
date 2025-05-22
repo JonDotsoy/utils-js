5c74aa02f488d85b2f84752c7ff4890d90f5a23c
Jonathan Delgado <hi@jon.soy>
2025-05-21 21:40:33 -0400

    fix(query): correct assignment of selectProps in where method

--- src/query/query.ts
+++ src/query/query.ts
@@ -37,7 +37,7 @@ export default class Query<T> {
 
       return predicateResult;
     });
-    query.#selectProps = [...query.#selectProps];
+    query.#selectProps = [...this.#selectProps];
     return query;
   }
 
