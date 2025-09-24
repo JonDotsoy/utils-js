46ed41a21eece934b0d5f2668bbb3212956a3794
Jonathan Delgado <hi@jon.soy>
2025-09-24 17:17:29 -0300

    fix: remove SymbolResult export from package and index

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,3 @@ export { set } from "./set/set.js";
 export { visit } from "./visit/visit.js";
 export { pipe } from "./pipe/pipe.js";
 export { result } from "./result/result.js";
-export { SymbolResult } from "./symbol-result/symbol-result.js";
